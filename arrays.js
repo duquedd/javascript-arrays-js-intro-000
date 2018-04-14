0a1e49c3ac6ffd533a8f1da7cdcdac6fd134946b
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-14 23:55:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = [`snickers`,`hundred grand`,`kitkat`,`skittles`]
 
 function addElementToBeginningOfArray(array,frontElement){
   return [frontElement,...array]
-  
+
 }
 function destructivelyAddElementToBeginningOfArray(array,frontElement){
   return array=[frontElement,...array]
