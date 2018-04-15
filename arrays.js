a7fd6de436d40573a878489c67f38b29446988f9
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-15 00:03:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,10 @@ function destructivelyAddElementToBeginningOfArray(array,frontElement){
   array.unshift(frontElement)
   return array
 }
+function addElementToEndOfArray(array,frontElement){
+  return [frontElement,...array]
+}
+function destructivelyAddElementToEndOfArray(array,frontElement){
+  array.push(frontElement)
+  return array
+}
